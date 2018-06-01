4bacc0a9c74fd49e498b18682c0d2995a9848434
M4xw <m4x@m4xw.net>
2018-06-01 02:02:39 +0200

    WIP Audio fixes (working on 5x)

--- audio/drivers/switch_audio.c
+++ audio/drivers/switch_audio.c
@@ -22,8 +22,6 @@
 #include "../audio_driver.h"
 #include "../../verbosity.h"
 
-#define ALIGN_VAL(x, a) (((x) + ((a)-1)) & ~((a)-1))
-
 typedef struct
 {
       bool blocking;
@@ -34,27 +32,37 @@ typedef struct
       AudioOutBuffer *current_buffer;
 } switch_audio_t;
 
+#define SAMPLERATE 48000
+#define CHANNELCOUNT 2
+#define FRAMERATE (1000 / 200) // this is upped
+#define SAMPLECOUNT (SAMPLERATE / FRAMERATE)
+#define BYTESPERSAMPLE 2
+
+static uint32_t switch_audio_data_size()
+{
+      return (SAMPLECOUNT * CHANNELCOUNT * BYTESPERSAMPLE);
+}
+
 static size_t switch_audio_buffer_size(void *data)
 {
       (void)data;
-      size_t sample_buffer_size = ((audoutGetSampleRate() * audoutGetChannelCount() * sizeof(uint16_t)) + 0xfff) & ~0xfff;
-      return ALIGN_VAL(sample_buffer_size, 0x1000);
+      return (switch_audio_data_size() + 0xfff) & ~0xfff;
 }
 
-// TODO: Needs testing
 static ssize_t switch_audio_write(void *data, const void *buf, size_t size)
 {
-      return size;
       size_t to_write = size;
       switch_audio_t *swa = (switch_audio_t *)data;
+
+      if (!swa)
+            return 0;
 
       if (!swa->current_buffer)
       {
             uint32_t num;
             if (R_FAILED(audoutGetReleasedAudioOutBuffer(&swa->current_buffer, &num)))
             {
-                  RARCH_LOG("Failed to get released buffer?\n");
-                  return -1;
+                  return 0;
             }
 
             if (num < 1)
@@ -64,12 +72,11 @@ static ssize_t switch_audio_write(void *data, const void *buf, size_t size)
                   if (swa->blocking)
                   {
                         RARCH_LOG("No buffer, blocking...\n");
+                        num = 0;
 
-                        while (swa->current_buffer == NULL)
-                        {
-                              // TODO: Fix Blocking
-                              return -1;
-                        }
+                        audoutWaitPlayFinish(&swa->current_buffer, &num, 33333333);
+                        if (swa->current_buffer == NULL)
+                              return 0;
                   }
                   else
                   {
@@ -80,23 +87,16 @@ static ssize_t switch_audio_write(void *data, const void *buf, size_t size)
             swa->current_buffer->data_size = 0;
       }
 
-      if (to_write > switch_audio_buffer_size(NULL) - swa->current_buffer->data_size)
-            to_write = switch_audio_buffer_size(NULL) - swa->current_buffer->data_size;
-
-      memcpy(((uint8_t *)swa->current_buffer->buffer) + swa->current_buffer->data_size, buf, to_write);
-      swa->current_buffer->data_size += to_write;
+      memcpy(swa->current_buffer->buffer, buf, to_write);
+      swa->current_buffer->data_size = to_write;
       swa->current_buffer->buffer_size = switch_audio_buffer_size(NULL);
 
-      if (swa->current_buffer->data_size > (48000 * swa->latency) / 1000)
+      Result r = audoutAppendAudioOutBuffer(swa->current_buffer);
+      if (R_FAILED(r))
       {
-            Result r = audoutAppendAudioOutBuffer(swa->current_buffer);
-            if (R_FAILED(r))
-            {
-                  printf("failed to append buffer: 0x%x\n", r);
-                  return -1;
-            }
-            swa->current_buffer = NULL;
+            return 0;
       }
+      swa->current_buffer = NULL;
 
       swa->last_append = svcGetSystemTick();
 
@@ -105,7 +105,6 @@ static ssize_t switch_audio_write(void *data, const void *buf, size_t size)
 
 static bool switch_audio_stop(void *data)
 {
-      return true;
       switch_audio_t *swa = (switch_audio_t *)data;
       if (!swa)
             return false;
@@ -125,8 +124,9 @@ static bool switch_audio_stop(void *data)
 
 static bool switch_audio_start(void *data, bool is_shutdown)
 {
-      return true;
       switch_audio_t *swa = (switch_audio_t *)data;
+      if (!swa)
+            return false;
 
       if (swa->is_paused)
       {
@@ -143,7 +143,6 @@ static bool switch_audio_start(void *data, bool is_shutdown)
 
 static bool switch_audio_alive(void *data)
 {
-      return true;
       switch_audio_t *swa = (switch_audio_t *)data;
       if (!swa)
             return false;
@@ -152,11 +151,16 @@ static bool switch_audio_alive(void *data)
 
 static void switch_audio_free(void *data)
 {
-      return;
       switch_audio_t *swa = (switch_audio_t *)data;
 
+      if (swa)
+      {
+            if(!swa->is_paused)
+                  audoutStopAudioOut();
 
-      free(swa);
+            audoutExit();
+            free(swa);
+      }
 }
 
 static bool switch_audio_use_float(void *data)
@@ -167,7 +171,6 @@ static bool switch_audio_use_float(void *data)
 
 static size_t switch_audio_write_avail(void *data)
 {
-      return 0;
       switch_audio_t *swa = (switch_audio_t *)data;
 
       if (!swa || !swa->current_buffer)
@@ -178,7 +181,6 @@ static size_t switch_audio_write_avail(void *data)
 
 static void switch_audio_set_nonblock_state(void *data, bool state)
 {
-      return;
       switch_audio_t *swa = (switch_audio_t *)data;
 
       if (swa)
@@ -190,7 +192,6 @@ static void *switch_audio_init(const char *device,
                                unsigned block_frames,
                                unsigned *new_rate)
 {
-      return NULL;
       switch_audio_t *swa = (switch_audio_t *)calloc(1, sizeof(*swa));
       printf("Init Audio..\n");
       if (!swa)
@@ -209,23 +210,16 @@ static void *switch_audio_init(const char *device,
             goto cleanExit;
       }
 
-      // Set audio rate
-      *new_rate = audoutGetSampleRate();
-
       // Create Buffers
       for (int i = 0; i < 3; i++)
       {
-            swa->buffers[i].next = &swa->buffers[i];
+            swa->buffers[i].next = NULL;
             swa->buffers[i].buffer = memalign(0x1000, switch_audio_buffer_size(NULL));
             swa->buffers[i].buffer_size = switch_audio_buffer_size(NULL);
-            swa->buffers[i].data_size = switch_audio_buffer_size(NULL);
+            swa->buffers[i].data_size = switch_audio_data_size();
             swa->buffers[i].data_offset = 0;
 
-            if (swa->buffers[i].buffer == NULL)
-            {
-                  // TODO: we might memory leak here, but it doesn't really matter
-                  goto cleanExit;
-            }
+           memset(swa->buffers[i].buffer, 0, switch_audio_buffer_size(NULL));
 
             /* 
                This might be the cause of the Audio noise at load
@@ -238,19 +232,23 @@ static void *switch_audio_init(const char *device,
             }
       }
 
+      // Set audio rate
+      *new_rate = SAMPLERATE;
+
       swa->is_paused = true;
       swa->current_buffer = NULL;
       swa->latency = latency;
       swa->last_append = svcGetSystemTick();
+      swa->blocking = block_frames;
 
       return swa;
 
 cleanExit:;
-      free(swa);
+
+      if (swa)
+            free(swa);
+
       printf("Something failed in Audio Init!\n");
-      
-      while(1)
-            ;
 
       return NULL;
 }
