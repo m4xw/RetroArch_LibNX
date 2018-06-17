9245cd7ad6590bcd1ebaf1eb0b030987fa5783da
M4xw <m4x@m4xw.net>
2018-06-17 19:10:14 +0200

    [Config] Set Audio and VSync on as default

--- configuration.c
+++ configuration.c
@@ -1693,7 +1693,8 @@ static void config_set_defaults(void)
       video_driver_set_threaded(g_defaults.settings.video_threaded_enable);
 #else
       video_driver_set_threaded(true);
-      configuration_set_bool(settings, settings->bools.video_vsync, false);
+      configuration_set_bool(settings, settings->bools.video_vsync, true);
+      configuration_set_bool(settings, settings->bools.audio_sync, true);
 #endif
    settings->floats.video_msg_color_r          = ((message_color >> 16) & 0xff) / 255.0f;
    settings->floats.video_msg_color_g          = ((message_color >>  8) & 0xff) / 255.0f;
